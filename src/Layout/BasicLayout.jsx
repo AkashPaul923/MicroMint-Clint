b2e21fcb6730ff92a3a4a49490b7fde50abcf2d7
AkashPaul923 <akashpaul923@gmail.com>
2025-01-19 19:04:55 +0600

    Navbar design

---
+++ src/Layout/BasicLayout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Components/Navigation/Navbar";
+
+const BasicLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            BasicLayout
+        </div>
+    );
+};
+
+export default BasicLayout;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import BasicLayout from "../Layout/BasicLayout";
+import DashboardLayout from "../Layout/DashboardLayout";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <p>Basic Layout</p>
-    }
+        element: <BasicLayout></BasicLayout>,
+    },
+    {
+        path: "/dashboard",
+        element: <DashboardLayout></DashboardLayout>,
+    },
 ])
 
 export default router;
